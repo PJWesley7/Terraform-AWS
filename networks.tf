080a66a609f43714daf7abb9ec1de4de85c1f1ac
pjwesley <pjwesley.chintalapudi@gmail.com>
2022-01-07 17:29:58 +0530

    added subnets

--- networks.tf
+++ networks.tf
@@ -1,6 +1,46 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.default_cidr
-  tags = {
-      name = "my_first_vpc"
+resource "aws_vpc" "first_vpc" {
+  cidr_block            = var.default_cidr
+  tags                  = {
+      name              = "my_first_vpc"
+  }
+}
+
+resource "aws_subnet" "first_subnet" {
+  vpc_id                = aws_vpc.first_vpc.id
+  cidr_block            = var.subnet_range[0]
+  availability_zone     = "ap-south-1a"
+
+  tags                  = {
+    Name                = var.subnet_names[0]
+  }
+}
+
+resource "aws_subnet" "second_subnet" {
+  vpc_id                = aws_vpc.first_vpc.id
+  cidr_block            = var.subnet_range[1]
+  availability_zone     = "ap-south-1b"
+
+  tags                  = {
+    Name                = var.subnet_names[1]
+  }
+}
+
+resource "aws_subnet" "third_subnet" {
+  vpc_id                = aws_vpc.first_vpc.id
+  cidr_block            = var.subnet_range[2]
+  availability_zone     = "ap-south-1c"
+
+  tags                  = {
+    Name                = var.subnet_names[2]
+  }
+}
+
+resource "aws_subnet" "fourth_subnet" {
+  vpc_id                = aws_vpc.first_vpc.id
+  cidr_block            = var.subnet_range[3]
+  availability_zone     = "ap-south-1b"
+
+  tags                  = {
+    Name                = var.subnet_names[3]
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "default_cidr" {
     default = "12.12.0.0/16"
 }
+
+variable "subnet_range"{
+    default = ["12.12.0.0/24","12.12.1.0/24","12.12.2.0/24","12.12.3.0/24"]
+}
+
+variable "subnet_names"{
+    default = ["web-1","web-2","db-1","db-2"]
+}
