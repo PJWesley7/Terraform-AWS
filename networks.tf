bd56a26da0eff5742b6872c079dc665f638d3f35
pjwesley <pjwesley.chintalapudi@gmail.com>
2022-01-07 18:36:42 +0530

    added count varaible

--- networks.tf
+++ networks.tf
@@ -5,42 +5,13 @@ resource "aws_vpc" "first_vpc" {
   }
 }
 
-resource "aws_subnet" "first_subnet" {
+resource "aws_subnet" "subnets" {
+  count                 = length(var.subnet_range)
   vpc_id                = aws_vpc.first_vpc.id
-  cidr_block            = var.subnet_range[0]
+  cidr_block            = var.subnet_range[count.index]
   availability_zone     = "ap-south-1a"
 
   tags                  = {
-    Name                = var.subnet_names[0]
-  }
-}
-
-resource "aws_subnet" "second_subnet" {
-  vpc_id                = aws_vpc.first_vpc.id
-  cidr_block            = var.subnet_range[1]
-  availability_zone     = "ap-south-1b"
-
-  tags                  = {
-    Name                = var.subnet_names[1]
-  }
-}
-
-resource "aws_subnet" "third_subnet" {
-  vpc_id                = aws_vpc.first_vpc.id
-  cidr_block            = var.subnet_range[2]
-  availability_zone     = "ap-south-1c"
-
-  tags                  = {
-    Name                = var.subnet_names[2]
-  }
-}
-
-resource "aws_subnet" "fourth_subnet" {
-  vpc_id                = aws_vpc.first_vpc.id
-  cidr_block            = var.subnet_range[3]
-  availability_zone     = "ap-south-1b"
-
-  tags                  = {
-    Name                = var.subnet_names[3]
+    Name                = var.subnet_names[count.index]
   }
 }
