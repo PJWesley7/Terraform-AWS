c7f2906fd39269364107a1c97c7fe3bed0857b6a
pjwesley <pjwesley.chintalapudi@gmail.com>
2022-01-07 18:13:42 +0530

    added description for varaibles

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "default_cidr" {
-    default = "12.12.0.0/16"
+    default     = "12.12.0.0/16"
+    description = "This is the default cidr"
 }
 
 variable "subnet_range"{
-    default = ["12.12.0.0/24","12.12.1.0/24","12.12.2.0/24","12.12.3.0/24"]
+    default     = ["12.12.0.0/24","12.12.1.0/24","12.12.2.0/24","12.12.3.0/24"]
+    description = "These are subnet ranges"
 }
 
 variable "subnet_names"{
-    default = ["web-1","web-2","db-1","db-2"]
+    default     = ["web-1","web-2","db-1","db-2"]
+    description = "These are subnet names"
 }
